eaf55d161bfa73067a33d25b97605d099ee6d0fc
AndrewClimber <seoclimber@gmail.com>
2019-09-27 05:12:31 +0700

    2019.09.27

---
+++ ch06/ex2-drop/lib/list_drop.ex
@@ -0,0 +1,17 @@
+defmodule ListDrop do
+  @moduledoc """
+  Documentation for ListDrop.
+  """
+  def falls(list) do
+    falls(list, [])
+  end
+
+  def falls([], results) do
+    Enum.reverse(results)
+  end
+
+  def falls([head|tail], results) do
+    falls(tail, [Drop.fall_velocity(head) | results])
+  end
+
+end
