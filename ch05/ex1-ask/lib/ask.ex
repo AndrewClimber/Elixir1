177dc3119cca947790f52c373d3f9f61f62ec439
AndrewClimber <seoclimber@gmail.com>
2019-09-26 22:55:28 +0700

    2019.09.26

---
+++ ch05/ex1-ask/lib/ask.ex
@@ -0,0 +1,18 @@
+defmodule Ask do
+  @moduledoc """
+  ask example
+  """
+
+  def chars() do
+    IO.puts("""
+    Which planemo are you on?
+     1. Earth
+     2. Moon
+     3. Mars
+    """)
+
+    IO.getn("Which? > ")
+
+  end
+
+end
