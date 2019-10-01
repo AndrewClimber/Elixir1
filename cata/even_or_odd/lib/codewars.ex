8dfb37feb2e33ac69a234c46dc5818284555d1df
AndrewClimber <seoclimber@gmail.com>
2019-10-01 09:51:57 +0700

    rr

--- cata/even_or_odd/lib/codewars.ex
+++ cata/even_or_odd/lib/codewars.ex
@@ -4,13 +4,6 @@ and returns "Even" for even numbers or "Odd" for odd numbers.
 """
 defmodule Codewars do
 import Integer
-
-  def even_or_odd(number) when is_even(number) do
-    "Even"
-  end
-
-  def even_or_odd(number) when is_odd(number) do
-    "Odd"
-  end
-
+  def even_or_odd(number) when is_even(number) do  "Even" end
+  def even_or_odd(number) when is_odd(number)  do  "Odd"  end
 end
